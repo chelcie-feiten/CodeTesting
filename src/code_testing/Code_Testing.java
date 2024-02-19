1cabb0ccba297fb4eefebad7f4b73f572f6fa7aa
Chelcie Feiten <chelcie.feiten@outlook.com>
2024-02-19 13:01:09 -0600

    Initial Commit To Test Branching

---
+++ src/code_testing/Code_Testing.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (C) 2022 Northwood Technical College
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+package code_testing;
+
+
+/**
+ *
+ * @author Chelcie Feiten
+ */
+public class Code_Testing {
+
+    public static void main(String[] args) {
+        System.out.print("hello world");
+    }
+
+            
+    
+}//end main class
